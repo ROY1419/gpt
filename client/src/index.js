923ac9b2fbefe572e19f04f337481546f57d4b26
Roy1419 <royshubham471@gmail.com>
2024-06-19 16:04:20 +0530

    carete some folder & showing some error after fix it the application will run till date stay update

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store} >
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID} >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </Provider>
+  </React.StrictMode>,
+)
